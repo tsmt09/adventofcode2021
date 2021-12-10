c17af301fe7557399ae7d51e6cc3ace867ef3c86
Tobias Schmitt <tschmittldk@gmail.com>
2021-12-10 20:29:36 +0100

    init

---
+++ src/bin/day1.rs
@@ -0,0 +1,58 @@
+use std::io;
+use std::io::BufRead;
+use std::io::Lines;
+use std::io::StdinLock;
+
+fn get_vector_from_lines(lines: Lines<StdinLock<>>) -> Vec<i32> {
+    let mut int_vec: Vec<i32> = Vec::new();
+    for line in lines {
+        match line {
+            Ok(str_line) => {
+                int_vec.push(str_line.parse().unwrap());
+            }
+            Err(err) => {
+                println!("error: {}", err)
+            }
+        }
+    }
+    int_vec
+}
+
+fn measure_increases(vec: Vec<i32>) -> i32 {
+    let mut last_value: i32 = -1;
+    let mut increased_number: i32 = -1;
+    for element in vec {
+        if element > last_value {
+            increased_number += 1;
+        }
+        last_value = element;
+    }
+    increased_number
+}
+
+fn three_measurement(vec: Vec<i32>) -> i32 {
+    let mut new_vec: Vec<i32> = Vec::new();
+    for vec_index in 0..vec.len() {
+        let mut sum = 0;
+        sum += vec[vec_index];
+        if (vec_index + 1) < (vec.len() - 1) {
+            sum += vec[vec_index + 1];
+        }
+        if (vec_index + 2) < (vec.len() - 1) {
+            sum += vec[vec_index + 2];
+        }
+        new_vec.push(sum);
+    }
+    measure_increases(new_vec)
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    let vector = get_vector_from_lines(lines);
+    let vector_tree = vector.clone();
+    let increases = measure_increases(vector);
+    println!("Increased number count: {}", increases);
+    let three_increases = three_measurement(vector_tree);
+    println!("Three increases number count: {}", three_increases);
+}
