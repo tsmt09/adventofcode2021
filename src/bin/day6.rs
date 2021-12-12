186f3344ab22ee6bb1f9513a5c6424dbfe6e6b35
Tobias Schmitt <tschmittldk@gmail.com>
2021-12-12 21:53:46 +0000

    refactored solution - nice ride!

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,114 +1,90 @@
 extern crate utilities;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use std::thread;
 
 #[derive(Debug, Clone, Copy)]
-struct Lanternfish {
-    birth_timer: u8
+struct LanternfishCrowd {
+    days_left_count: [u128; 9]
 }
 
-impl Lanternfish {
-    fn new(bt: u8) -> Lanternfish {
-        Lanternfish {
-            birth_timer: bt 
+impl LanternfishCrowd {
+    fn new(dlc: [u128; 9]) -> LanternfishCrowd {
+        LanternfishCrowd {
+            days_left_count: dlc
         }
     }
-    fn evolve(&mut self) -> Vec<Lanternfish> {
-        let mut children: Vec<Lanternfish> = Vec::new();
-        if self.birth_timer == 0 {
-            self.birth_timer = 6;
-            children.push(Lanternfish::new(8));
-        } else {
-            self.birth_timer -= 1;
-        }
-        children
+    fn spend_day(&mut self) {
+        let copy = self.days_left_count.clone();
+        self.days_left_count[8] = copy[0];
+        self.days_left_count[7] = copy[8];
+        self.days_left_count[6] = copy[7] + copy[0];
+        self.days_left_count[5] = copy[6];
+        self.days_left_count[4] = copy[5];
+        self.days_left_count[3] = copy[4];
+        self.days_left_count[2] = copy[3];
+        self.days_left_count[1] = copy[2];
+        self.days_left_count[0] = copy[1];
+
+    }
+    fn allover_count(&self) -> u128 {
+        return self.days_left_count[0] 
+                + self.days_left_count[1]
+                + self.days_left_count[2]
+                + self.days_left_count[3]
+                + self.days_left_count[4]
+                + self.days_left_count[5]
+                + self.days_left_count[6]
+                + self.days_left_count[7]
+                + self.days_left_count[8]; 
     }
 }
 
-fn parse_startfishes(input: Vec<String>) -> Vec<Lanternfish> {
-    let mut startfishes: Vec<Lanternfish> = Vec::new();
+fn parse_startfishes(input: Vec<String>) -> LanternfishCrowd {
+    let mut crowd = LanternfishCrowd::new([0u128,0,0,0,0,0,0,0,0]);
     if !input.is_empty() {
         let lifetimes: Vec<u8> = input[0]
                         .split(',')
                         .map(|e| e.parse::<u8>().unwrap())
                         .collect();
         for lt in lifetimes {
-            startfishes.push(Lanternfish::new(lt));
+            match lt {
+                0 => crowd.days_left_count[0] += 1,
+                1 => crowd.days_left_count[1] += 1,
+                2 => crowd.days_left_count[2] += 1,
+                3 => crowd.days_left_count[3] += 1,
+                4 => crowd.days_left_count[4] += 1,
+                5 => crowd.days_left_count[5] += 1,
+                6 => crowd.days_left_count[6] += 1,
+                7 => crowd.days_left_count[7] += 1,
+                _ => panic!("unknown start number {}", lt)
+            }
         }
     }
-    startfishes
+    crowd
 }
 
-fn simulate_lifetime(fishes: Vec<Lanternfish>, years: u32, threads: usize, chunksize: usize) -> Vec<Lanternfish> {
-    let mut fishes_work = fishes.clone();
-    for day in 0..years {
-        println!("Day {}, Fishes: {}", day, fishes_work.len());
-        /* just working storage to save the during chunk computation */
-        let mut fish_work_store: Vec<Lanternfish> = Vec::new();
-        while !fishes_work.is_empty() {
-            let mut t_vec: Vec<thread::JoinHandle<Vec<Lanternfish>>> = Vec::new();
-            /* spawn N threads working on chunksize X */
-            for _ in 0..threads {
-                let mut split: Vec<Lanternfish>;
-                if fishes_work.len() >= chunksize.try_into().unwrap() {
-                    split = fishes_work.split_off((fishes_work.len() as usize) - chunksize);
-                } else {
-                    split = fishes_work.split_off(0);
-                }
-                /* computation in closure */
-                let t_handle = std::thread::spawn(move || -> Vec<Lanternfish> {
-                    let mut newfishes: Vec<Lanternfish> = Vec::new();
-                    for fish in split.iter_mut() {
-                        let mut born = fish.evolve();
-                        if !born.is_empty() {
-                            newfishes.append(&mut born);
-                        }
-                    }
-                    if !newfishes.is_empty() {
-                        split.append(&mut newfishes);
-                    }
-                    split
-                });
-                t_vec.push(t_handle);
-            }
-            for t in t_vec {
-                let mut result = t.join().unwrap();
-                fish_work_store.append(&mut result);
-            }
-        }
-        fishes_work.append(&mut fish_work_store);
+fn simulate_lifetime(crowd: LanternfishCrowd, days: u128) -> LanternfishCrowd {
+    let mut work_crowd = crowd;
+    for day in 0..days {
+        println!("Day {} - Fishes {} - crowd: {:?}", day, work_crowd.allover_count(), work_crowd.days_left_count);
+        work_crowd.spend_day();
     }
-    fishes_work
+    work_crowd
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "day3", version = "1.0.0")]
 struct Args {
     #[structopt(short = "f", long, parse(from_os_str))]
-    file: PathBuf,
-    #[structopt(short = "t", long)]
-    threads: Option<usize>,
-    #[structopt(short = "c", long)]
-    chunksize: Option<usize>
+    file: PathBuf
 }
 
 fn main() {
-    let args = Args::from_args();
-
-    let threads = match args.threads {
-        Some(n) => n,
-        None => 2
-    };
-    let chunksize = match args.chunksize {
-        Some(n) => n,
-        None => 1000000
-    };
-
+    let args = Args::from_args();  
     let vec_str = utilities::files::open_file(args.file);
-    let mut fishes = parse_startfishes(vec_str);
-    fishes = simulate_lifetime(fishes, 256, threads, chunksize);
-    println!("Result: {}", fishes.len());
+    let mut crowd = parse_startfishes(vec_str);
+    crowd = simulate_lifetime(crowd, 256);
+    println!("Result: {}", crowd.allover_count());
 }
 
 #[cfg(test)]
@@ -120,13 +96,12 @@ mod tests {
         let testvec: Vec<String> = vec![
             "3,4,3,1,2".to_string(),
         ];
-        let mut fishes = parse_startfishes(testvec);
-        assert_eq!(5, fishes.len());
-        fishes = simulate_lifetime(fishes, 18, 1, 1);
-        assert_eq!(26, fishes.len());
-        fishes = simulate_lifetime(fishes, 80-18, 1, 1);
-        assert_eq!(5934, fishes.len());
-        fishes = simulate_lifetime(fishes, 265-80, 8, 2000000);
-        assert_eq!(26984457539, fishes.len());
+        let mut crowd = parse_startfishes(testvec);
+        crowd = simulate_lifetime(crowd, 18);
+        assert_eq!(26, crowd.allover_count());
+        crowd = simulate_lifetime(crowd, 80-18);
+        assert_eq!(5934, crowd.allover_count());
+        crowd = simulate_lifetime(crowd, 256-80);
+        assert_eq!(26984457539, crowd.allover_count());
     }
 }
